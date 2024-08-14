8c2bee36a24695e8c602c97ebd167a673e162d05
John Doe <aashirisrar968@gmail.com>
2024-08-14 11:55:03 +0500

    completed call to action section

--- app/sections/CallToAction.tsx
+++ app/sections/CallToAction.tsx
@@ -1,3 +1,24 @@
+"use client";
+
+import Button from "@/components/Button";
+import Heading from "@/components/Heading";
+
 export const CallToAction = () => {
-  return null;
+  return (
+    <section className="xl:px-96 lg:px-20 py-[96px] px-5 bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF]">
+      <Heading
+        heading="Sign up for free today"
+        subHeading="Celebrate the joy of accomplishment with an app designed to track your progress and motivate your efforts."
+      />
+      <div className="flex items-center justify-center gap-2 pt-[40px]">
+        <Button onClick={() => {}} title="Get for free" />
+        <Button
+          className="bg-transparent text-black"
+          onClick={() => {}}
+          title="Learn More"
+          arrow
+        />
+      </div>
+    </section>
+  );
 };
