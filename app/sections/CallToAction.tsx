08373e965cebeb486b613684a6758e78b2fdfe91
John Doe <aashirisrar968@gmail.com>
2024-08-14 15:08:40 +0500

    added images in cta section

--- app/sections/CallToAction.tsx
+++ app/sections/CallToAction.tsx
@@ -1,15 +1,36 @@
 "use client";
 
+import Image from "next/image";
+
+import starImage from "@/public/assets/star.png";
+import springImage from "@/public/assets/spring.png";
+
 import Button from "@/components/Button";
 import Heading from "@/components/Heading";
 
 export const CallToAction = () => {
   return (
-    <section className="xl:px-96 lg:px-20 py-[96px] px-5 bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF]">
-      <Heading
-        heading="Sign up for free today"
-        subHeading="Celebrate the joy of accomplishment with an app designed to track your progress and motivate your efforts."
-      />
+    <section className="overflow-x-clip xl:px-96 lg:px-20 py-[96px] px-5 bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF]">
+      <div className="relative">
+        <Heading
+          heading="Sign up for free today"
+          subHeading="Celebrate the joy of accomplishment with an app designed to track your progress and motivate your efforts."
+        />
+        <Image
+          src={starImage}
+          width={360}
+          height={360}
+          alt="star Image"
+          className="hidden md:block absolute -left-[250px] -top-[137px]"
+        />
+        <Image
+          src={springImage}
+          width={360}
+          height={360}
+          alt="spring Image"
+          className="hidden md:block absolute -right-[231px] -top-[19px]"
+        />
+      </div>
       <div className="flex items-center justify-center gap-2 pt-[40px]">
         <Button onClick={() => {}} title="Get for free" />
         <Button
