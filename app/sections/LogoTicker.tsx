9757592b09a85f800f5112907bb8f8c4c69b32cd
John Doe <aashirisrar968@gmail.com>
2024-08-14 15:40:28 +0500

    fixed logo ticker

--- app/sections/LogoTicker.tsx
+++ app/sections/LogoTicker.tsx
@@ -10,7 +10,7 @@ import apexLogo from "@/public/assets/logo-apex.png";
 export const LogoTicker = () => {
   return (
     <div className="xl:px-96 px-5 py-8 md:py-12 bg-white">
-      <div className="flex overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)]">
+      <div className="flex items-center justify-center overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)]">
         <div className="flex gap-14 flex-none *:h-8 *:w-auto">
           <Image src={acmeLogo} alt="acme Logo" />
           <Image src={quantumLogo} alt="quantum Logo" />
