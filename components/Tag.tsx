2aec708b1b72e485957ca22b68f5046150762781
John Doe <aashirisrar968@gmail.com>
2024-08-14 16:18:50 +0500

    added animation to popular tag on pricing page

--- components/Tag.tsx
+++ components/Tag.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { cn } from "@/libs/utls";
+import { easeInOut, motion } from "framer-motion";
 
 interface TagProps {
   title: string;
@@ -9,16 +10,25 @@ interface TagProps {
 
 const Tag: React.FC<TagProps> = ({ title, popular }) => {
   return (
-    <div
+    <motion.div
+      animate={{
+        backgroundPositionX: "-100%",
+      }}
+      transition={{
+        repeat: Infinity,
+        ease: "linear",
+        repeatType: "loop",
+        duration: 1,
+      }}
       className={cn(
         "leading-5 font-medium text-sm tracking-tight border border-[#222222]/10 inline-flex px-3 py-1 rounded-[8px]",
         popular
-          ? "bg-clip-text text-transparent bg-[linear-gradient(to_right,#DD7DDF,#E1CD86,#8BCB92,#71C2EF,#3BFFFF,#DD7DDF)] border-white/20"
+          ? "bg-clip-text text-transparent bg-[linear-gradient(to_right,#DD7DDF,#E1CD86,#8BCB92,#71C2EF,#3BFFFF,#DD7DDF,#E1CD86,#8BCB92,#71C2EF,#3BFFFF,#DD7DDF)] [background-size:200%] border-white/20"
           : ""
       )}
     >
       {title}
-    </div>
+    </motion.div>
   );
 };
 
